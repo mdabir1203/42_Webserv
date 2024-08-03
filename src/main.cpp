ec35f59c50958b2b4ebe3974c6b66590e413d55b
mdabir1203 <md.abir1203@gmail.com>
2024-08-03 23:03:54 +0500

    added post method and tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,56 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <sstream>
 #include <cstdio>
+#include <fstream>
 
-#define PORT 5500
 #define BUFFER_SIZE 1024
+#define MAX_BODY_SIZE 1024
 
 class HttpServer {
 public:
-    HttpServer(int port) : port_(port) {}
+    HttpServer(const std::vector<int>& ports, const std::map<std::string, std::string>& hostnames)
+        : ports_(ports), hostnames_(hostnames) {}
 
     void start() {
-        // Create socket
+        for (size_t i = 0; i < ports_.size(); ++i) {
+            int port = ports_[i];
+            startServer(port);
+        }
+    }
+
+private:
+    void startServer(int port) {
         int sockfd = socket(AF_INET, SOCK_STREAM, 0);
         if (sockfd < 0) {
             perror("socket");
             return;
         }
 
-        // Set address and port number for the server
         struct sockaddr_in server_addr;
         server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(port_);
+        server_addr.sin_port = htons(port);
         inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
 
-        // Bind the socket to the address and port
         if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
             perror("bind");
             return;
         }
 
-        // Listen for incoming connections
         if (listen(sockfd, 3) < 0) {
             perror("listen");
             return;
         }
 
-        std::cout << "Server listening on port " << port_ << std::endl;
+        std::cout << "Server listening on port " << port << std::endl;
 
         while (true) {
-            // Accept incoming connections
             struct sockaddr_in client_addr;
             socklen_t client_len = sizeof(client_addr);
             int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
@@ -53,15 +59,12 @@ public:
                 continue;
             }
 
-            // Handle client request
             handleRequest(client_sockfd);
 
-            // Close client socket
             close(client_sockfd);
         }
     }
 
-private:
     void handleRequest(int client_sockfd) {
         char buffer[BUFFER_SIZE];
         int bytes_received = recv(client_sockfd, buffer, BUFFER_SIZE, 0);
@@ -73,7 +76,6 @@ private:
         std::string request(buffer, bytes_received);
         std::cout << "Received request: " << request << std::endl;
 
-        // Parse HTTP request
         std::vector<std::string> request_lines;
         size_t pos = 0;
         while ((pos = request.find("\r\n")) != std::string::npos) {
@@ -81,30 +83,53 @@ private:
             request.erase(0, pos + 2);
         }
 
-        // Handle HTTP request
-        if (request_lines[0].find("GET") != std::string::npos) {
-            handleGetRequest(client_sockfd);
-        } else if (request_lines[0].find("POST") != std::string::npos) {
-            handlePostRequest(client_sockfd, request);
+        if (request_lines.empty()) {
+            sendError(client_sockfd, 400, "Bad Request");
+            return;
+        }
+
+        std::string method = request_lines[0].substr(0, request_lines[0].find(' '));
+        std::string path = request_lines[0].substr(request_lines[0].find(' ') + 1);
+        path = path.substr(0, path.find(' '));
+
+        if (method == "GET") {
+            handleGetRequest(client_sockfd, path);
+        } else if (method == "POST") {
+            if (request.size() > MAX_BODY_SIZE) {
+                sendError(client_sockfd, 413, "Payload Too Large");
+            } else {
+                handlePostRequest(client_sockfd, request);
+            }
         } else {
             sendError(client_sockfd, 405, "Method Not Allowed");
         }
     }
 
-    void handleGetRequest(int client_sockfd) {
-        // Send HTTP response
-        std::string response = "HTTP/1.1 200 OK\r\n\r\nHello, GET!";
-        send(client_sockfd, response.c_str(), response.size(), 0);
+void handleGetRequest(int client_sockfd, const std::string& path) {
+    std::string response;
+    if (path == "/") {
+        response = "HTTP/1.1 200 OK\r\n\r\nHello, GET!";
+    } else {
+        std::string filePath = "." + path; // Use std::string for concatenation
+        std::ifstream file(filePath.c_str()); // Convert to C-string for ifstream
+        if (file.is_open()) {
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            response = "HTTP/1.1 200 OK\r\n\r\n" + buffer.str();
+        } else {
+            sendError(client_sockfd, 404, "Not Found");
+            return;
+        }
     }
+    send(client_sockfd, response.c_str(), response.size(), 0);
+}
 
     void handlePostRequest(int client_sockfd, const std::string& request) {
-        // Extract the body of the POST request
         size_t body_pos = request.find("\r\n\r\n");
         std::string body = (body_pos != std::string::npos) ? request.substr(body_pos + 4) : "";
 
         std::cout << "Received POST body: " << body << std::endl;
 
-        // Send HTTP response
         std::string response = "HTTP/1.1 200 OK\r\n\r\nHello, POST!";
         send(client_sockfd, response.c_str(), response.size(), 0);
     }
@@ -112,15 +137,27 @@ private:
     void sendError(int client_sockfd, int code, const std::string& message) {
         std::ostringstream responseStream;
         responseStream << "HTTP/1.1 " << code << " " << message << "\r\n\r\n";
+        responseStream << "<html><body><h1>" << code << " " << message << "</h1></body></html>";
         std::string response = responseStream.str();
         send(client_sockfd, response.c_str(), response.size(), 0);
     }
 
-    int port_;
+    std::vector<int> ports_;
+    std::map<std::string, std::string> hostnames_;
 };
 
 int main() {
-    HttpServer server(PORT);
+    std::vector<int> ports;
+    ports.push_back(5500);
+    ports.push_back(5501);
+
+    std::map<std::string, std::string> hostnames;
+    hostnames["example.com"] = "127.0.0.1";
+
+    HttpServer server(ports, hostnames);
     server.start();
+    while (true) {
+        // Keep the main thread alive
+    }
     return 0;
 }
